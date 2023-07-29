9c7a302d7448374a2de2f3d3823d8107cc58df74
Akash Dey <aceakashdey@gmail.com>
2023-07-29 21:23:29 +0530

    Fixed the design of Login page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,25 +1,23 @@
 import { forwardRef } from "react";
 import { twMerge } from "tailwind-merge";
 
-const Button = forwardRef(function Button(
-  { children, disabled, styles },
-  props,
-  ref
-) {
-  return (
-    <button
-      ref={ref}
-      {...props}
-      className={twMerge(
-        disabled
-          ? "hidden md:block md:bg-[#E6E6E6] md:text-stone-400 px-[26px] py-[10px] rounded-full"
-          : "bg-hl-col px-[26px] py-[10px] rounded-full text-[white]",
-        styles
-      )}
-    >
-      {children}
-    </button>
-  );
+const Button = forwardRef(function Button({ children, disabled, styles }, props, ref) {
+    return (
+        <button
+            ref={ref}
+            {...props}
+            className={twMerge(
+                disabled
+                    ? "hidden md:block md:bg-[#E6E6E6] md:text-stone-400 px-[26px] py-[10px] rounded-full shadow"
+                    : "bg-hl-col px-[26px] py-[10px] rounded-full text-[white] shadow",
+                styles
+            )}
+        >
+            {children}
+        </button>
+    );
 });
 
 export default Button;
+
+// box-shadow: 0px 3px 8px -3px rgba(23, 70, 116, 0.50);
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -45,30 +45,34 @@ const LoginForm = () => {
             className="flex items-center flex-col"
             onSubmit={handleSubmit(onSubmit)}
         >
-            <div className="mb-12 relative">
+            <div className="mb-[34px] relative">
                 <input
                     autoFocus
-                    className="rounded-lg w-[340px] h-[48px] px-5 border border-[#B7B7B7] text-stone-600 focus:border-none focus:outline-none focus:ring-2 focus:ring-[#578598]"
+                    className="rounded-[12px] w-[340px] h-[48px] px-[18px] border border-[#B7B7B7] text-stone-600 focus:border-none focus:outline-none focus:ring-2 focus:ring-[#578598]"
                     placeholder="Enter your email here"
                     disabled={isLoading}
                     {...register("email")}
                 />
                 {errors.email || supabaseError ? (
-                    <p className="pl-2 pt-1 text-red-500 absolute">{errors.email?.message || "Invalid password or email"}</p>
+                    <p className="pl-[26px] pt-[4px] text-red-500 absolute lato-custom text-[14px] font-bold">
+                        {errors.email?.message || "Invalid password or email"}
+                    </p>
                 ) : null}
             </div>
             <div className="flex flex-col justify-center relative">
-                <div className="mb-14 relative">
+                <div className="mb-[34px] relative">
                     <input
                         id="password"
-                        className="rounded-lg w-[340px] h-[48px] px-5 border border-[#B7B7B7] text-stone-600 focus:border-none focus:outline-none focus:ring-2 focus:ring-[#578598]"
+                        className="rounded-[12px] w-[340px] h-[48px] px-5 border border-[#B7B7B7] text-stone-600 focus:border-none focus:outline-none focus:ring-2 focus:ring-[#578598]"
                         placeholder={`Enter your password here`}
                         type={isShownPassword ? "text" : "password"}
                         disabled={isLoading}
                         {...register("password")}
                     />
                     {errors.password || supabaseError ? (
-                        <p className="pl-2 pt-1 text-red-500 absolute">{errors.password?.message || "Invalid password or email"}</p>
+                        <p className="pl-[26px] pt-[4px] text-red-500 absolute lato-custom text-[14px] font-bold">
+                            {errors.password?.message || "Invalid password or email"}
+                        </p>
                     ) : null}
                 </div>
                 <div
@@ -88,18 +92,18 @@ const LoginForm = () => {
                     )}
                 </div>
             </div>
-            <div className="flex gap-3 mb-8">
+            <div className="flex gap-[6px] mb-[46px] lato-custom">
                 <p className="text-[#8C8C8C]">Don&lsquo;t have an account yet?</p>
                 <Link
                     to="/register"
                     className="text-[#174674] hover:text-[#0b2239]"
                 >
-                    Register Now!
+                    <strong>Register Now!</strong>
                 </Link>
             </div>
             <button
                 type="submit"
-                className="focus:bg-[##10304f] bg-[#174674] px-[26px] py-[10px] rounded-full text-[white]"
+                className="focus:bg-[#10304f] bg-[#174674] px-[26px] py-[10px] rounded-full text-[white]"
             >
                 {isLoading ? (
                     <SyncLoader
